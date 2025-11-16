4eaa75792c3806f2e5b3549789178894c15b6e93
banjun <banjun@gmail.com>
2025-11-17 01:21:22 +0900

    add multipeer sink to work with device and relay to multiple sinks from single source

--- DMXRealityKit/DMXHolder.swift
+++ DMXRealityKit/DMXHolder.swift
@@ -14,12 +14,15 @@ public final class DMXHolder: DMXHolderType {
     public let universe: UInt16
     public var dmx: DMX = .init()
     private var observation: Task<Void, Never>? {didSet {oldValue?.cancel()}}
+    public let multipeer = Multipeer()
     public init(universe: UInt16) {
         self.universe = universe
     }
     public func start() {
         observation = Task {
             await sink.start(universe: universe)
+            await sink.subscribeMultipeer(multipeer.receivedData)
+            multipeer.start()
             for await payload in await sink.payloadsSequence {
                 guard let dmx = payload[.init(integerLiteral: universe)]?.dmx else { continue }
                 self.dmx = dmx
@@ -28,6 +31,10 @@ public final class DMXHolder: DMXHolderType {
     }
     public func stop() {
         observation = nil
-        Task {await sink.stop(universe: universe)}
+        Task {
+            await sink.unsubscribeMultipper()
+            multipeer.stop()
+            await sink.stop(universe: universe)
+        }
     }
 }
